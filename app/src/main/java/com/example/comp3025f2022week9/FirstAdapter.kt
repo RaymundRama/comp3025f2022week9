d83cdc9c85543d34262b8d7d6d36240c69e18fd1
Raymund <71455069+RaymundRama@users.noreply.github.com>
2022-11-13 22:06:33 -0500

    trying to get the mutable list of tv show to update after inserting info

--- app/src/main/java/com/example/comp3025f2022week9/FirstAdapter.kt
+++ app/src/main/java/com/example/comp3025f2022week9/FirstAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FirstAdapter(private val dataSet: Array<TvShow>) :
+class FirstAdapter(private val dataSet: MutableList<TvShow>) :
     RecyclerView.Adapter<FirstAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
--- app/src/main/java/com/example/comp3025f2022week9/MainActivity.kt
+++ app/src/main/java/com/example/comp3025f2022week9/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.comp3025f2022week9
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,22 +14,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var addTVShowFAB: FloatingActionButton
+    var FavouriteTVShows = mutableListOf<TvShow>(
+        TvShow("House of the Dragon", "HBO"),
+        TvShow("Lord of the Rings", "Prime Video"),
+        TvShow("Andor", "Disney"),
+        TvShow("Severance", "AppleTV"),
+        TvShow("Star Trek: Strange New Worlds", "Paramount"))
 
-    @SuppressLint("MissingInflatedId")
+    lateinit var addTVShowFAB: FloatingActionButton
+    lateinit var firstAdapter: FirstAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var FavouriteTVShows = arrayOf<TvShow>(
-            TvShow("House of the Dragon", "HBO"),
-            TvShow("Lord of the Rings", "Prime Video"),
-            TvShow("Andor", "Disney"),
-            TvShow("Severance", "AppleTV"),
-            TvShow("Star Trek: Strange New Worlds", "Paramount"))
-
-        val firstAdapter: FirstAdapter = FirstAdapter(FavouriteTVShows)
+        firstAdapter = FirstAdapter(FavouriteTVShows)
         val recyclerView: RecyclerView = findViewById(R.id.FirstRecyclerView)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = firstAdapter
 
@@ -43,24 +46,22 @@ class MainActivity : AppCompatActivity() {
     {
         val dialogTitle = getString(R.string.dialog_title)
         val positiveButtonTitle = getString(R.string.add_tv_show)
-        val negativeButtonTitle = getString(R.string.cancel)
-
         val builder = AlertDialog.Builder(this)
-
-        val tvShowTitleEditText = findViewById<EditText>(R.id.tv_show_title_editText)
-
-        val studioNameEditText = findViewById<EditText>(R.id.Studio_Name_EditText)
+        val view = layoutInflater.inflate(R.layout.add_new_tv_show_item,null)
 
         builder.setTitle(dialogTitle)
-        builder.setView(R.layout.add_new_tv_show_item)
+        builder.setView(view)
 
-        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
-            dialog.dismiss()
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ -> dialog.dismiss()
+
+            val tvShowTitleEditText = findViewById<EditText>(R.id.tv_show_title_editText)
+            val studioNameEditText = findViewById<EditText>(R.id.Studio_Name_EditText)
+            val newTVShow = TvShow(tvShowTitleEditText.text.toString(), studioNameEditText.text.toString())
+
+            FavouriteTVShows.add(newTVShow)
+            firstAdapter.notifyItemInserted(FavouriteTVShows.size)
         }
 
-        builder.setNegativeButton(negativeButtonTitle){ dialog, _ ->
-            dialog.dismiss()
-        }
 
         builder.create().show()
     }
